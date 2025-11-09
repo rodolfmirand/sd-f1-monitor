67d09b3484448fd40cb95b010982eb671528c5b9
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-11-09 15:49:33 -0300

    Add CORS middleware to main.py for improved API access

--- svcp/main.py
+++ svcp/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from repository import listar_veiculos, obter_veiculo, pneus_com_alerta
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/veiculos")
 def get_veiculos():
