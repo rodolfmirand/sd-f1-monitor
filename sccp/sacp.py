942c94d6e0d1c7eabeb5ff68077bfafd5ae99bc0
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-11-14 17:42:05 -0300

    Refactor SACP service to use gRPC for telemetry data storage and remove MQTT integration

--- sccp/sacp.py
+++ sccp/sacp.py
@@ -1,17 +1,16 @@
-import paho.mqtt.client as mqtt
-import json, time, datetime, os
+import grpc
+from concurrent import futures
+import time, datetime, os
 from pymongo import MongoClient
-from models import CarData
+import f1_pb2
+import f1_pb2_grpc
 
 MONGO_HOST = os.environ.get("MONGO_HOST", "mongo")
 MONGO_PORT = int(os.environ.get("MONGO_PORT", 27017))
 DB_NAME = os.environ.get("MONGO_DB", "f1_db")
-
-MQTT_HOST = os.environ.get("MQTT_HOST", "mqtt-broker")
-MQTT_PORT = int(os.environ.get("MQTT_PORT", 1883))
+GRPC_PORT = int(os.environ.get("GRPC_PORT", 50051))
 
 client = MongoClient(f"mongodb://{MONGO_HOST}:{MONGO_PORT}/", serverSelectionTimeoutMS=5000)
-
 while True:
     try:
         client.admin.command("ping")
@@ -24,30 +23,48 @@ while True:
 db = client[DB_NAME]
 collection = db["telemetria"]
 
-ssacp_id = int(DB_NAME.split("_")[-1])
+try:
+    ssacp_id = int(DB_NAME.split("_")[-1])
+except:
+    ssacp_id = 1
 
-def msg(c, u, m):
-    d = json.loads(m.payload.decode())
-    obj = CarData.from_dict(d)
-    ts = datetime.datetime.now().strftime("%H:%M:%S")
-    collection.insert_one(obj.to_dict())
-    print(f"[{ts}] [SSACP {ssacp_id}] Dados armazenados: {obj.name} ({obj.team}) posição {obj.position:.2f}")
+class SACPServiceServicer(f1_pb2_grpc.SACPServiceServicer):
 
-c = mqtt.Client(f"sacp{ssacp_id}")
+    def StoreTelemetry(self, request, context):
+        obj_dict = {
+            "id": request.id,
+            "name": request.name,
+            "team": request.team,
+            "front_left": request.front_left,
+            "front_right": request.front_right,
+            "rear_left": request.rear_left,
+            "rear_right": request.rear_right,
+            "position": request.position,
+            "timestamp": request.timestamp
+        }
 
-while True:
-    try:
-        c.connect(MQTT_HOST, MQTT_PORT, 60)
-        print(f"[SSACP {ssacp_id}] Conectado ao broker!")
-        break
-    except:
-        print(f"[SSACP {ssacp_id}] Broker ainda não disponível... tentando novamente em 2s")
-        time.sleep(2)
+        try:
+            collection.insert_one(obj_dict)
+            ts = datetime.datetime.now().strftime("%H:%M:%S")
+            print(f"[{ts}] [SSACP {ssacp_id}] Dados armazenados: {request.name} ({request.team}) posição {request.position}")
+            
+            return f1_pb2.StoreReply(success=True)
+            
+        except Exception as e:
+            print(f"[SSACP {ssacp_id}] Erro ao inserir no BD: {e}")
+            return f1_pb2.StoreReply(success=False)
 
-topic = f"f1/ssacp/{ssacp_id}"
-c.subscribe(topic)
-print(f"[SSACP {ssacp_id}] Escutando tópico: {topic}")
 
-c.on_message = msg
-print(f"[SSACP {ssacp_id}] Aguardando dados...")
-c.loop_forever()
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    
+    f1_pb2_grpc.add_SACPServiceServicer_to_server(SACPServiceServicer(), server)
+    
+    server_address = f'[::]:{GRPC_PORT}'
+    server.add_insecure_port(server_address)
+    
+    print(f"[SSACP {ssacp_id}] Servidor gRPC iniciado. Escutando em {server_address}")
+    server.start()
+    server.wait_for_termination()
+
+serve()
